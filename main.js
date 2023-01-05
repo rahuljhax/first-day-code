f5f1b115b14cb57ef3d79cd5858f17dc6dc8c5d5
Rahul jha <rahuljha.189244@gmail.com>
2023-01-05 23:46:47 +0530

    element by react js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const heading  = document.createElement('h1');
-heading.innerHTML = 'Namaste React';
-const root = document.getElementById('root');
-root.appendChild(heading);
+const heading = React.createElement('h1',{},'Namaste React js');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
